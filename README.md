68c85a358e32c8906d209a763bffc2efeb4cbb57
Your Name <yourname@example.com>
2017-11-16 19:04:33 -0500

    Alexa skill approved, final edits

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Game On Track
 
-_Delivers single fact per user prompt. Identifies prompt message alone; operates accordingly._ 
-_Does not discern one voice from another, allowing multiple users._
+_Facts about the lyricism, wit and candor of a '90s rap song partially responsible for the upward trajectory of its artist's career._
 
+This skill delivers a single fact when prompted by user. Once a user requests an action from "game on track," Alexa identifies the command and operates accordingly. User voices are not distinguishable during a skill exchange which enables GameOnTrack to support multiple players.
 
+<IMG SRC="https://pbs.twimg.com/media/C6ebI47WMAEf7Wt.jpg" ALT="game on track" WIDTH=32 HEIGHT=32>
 
-Formerly available to users as _BricksAndChips_, this fact skill is devoted to Christopher Wallace and the game that, he alleged, made him an animal.
+A salute to Christopher Wallace and the earworm, released more than twenty years ago, advising businessmen of the best practices within their trade. An homage to the track itself, and the candid guidelines therein, GameOnTrack also includes the actual breadth of circumstances that inspired its penning.
 
-To prevent one's wig from being pushed back, Wallace curated a manual years ago to provide guidance on how to keep one's game on track. This skill provides not only the individual steps themselves, but also breadth of the circumstances that inspired its penning. Formerly presented as somewhat of a circumscribed curriculum, this skill seeks to empower its users with a deeper understanding of the raw motivations behind its creation and the subsequent scope of its worth.
+At once the skill conveys the unsuspecting scope of a song's worth, and showers users with track facts about the North Carolina-influenced demo that led to Biggie's big break.
+
 
 Sample Alexa Skill from https://github.com/alexa/skill-sample-nodejs-fact
--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ var Alexa = require('alexa-sdk');
 var APP_ID = "amzn1.ask.skill.80a891d4-1cf4-4c25-be0f-d35f21c5f489";
 
 var SKILL_NAME = "Game On Track";
-var GET_FACT_MESSAGE = "Here's your fact: ";
-var HELP_MESSAGE = "You can say tell me something good, or, you can say exit... What's it going to be?";
+var GET_FACT_MESSAGE = "Fact: ";
+var HELP_MESSAGE = "For a new fact, you can say, 'Tell me something!' If you wish to end Game On Track, say 'exit!'";
 var HELP_REPROMPT = "What can I help you with?";
-var STOP_MESSAGE = "Gotta go, gotta go. More pies to bake up; word up!";
+var STOP_MESSAGE = "It's a bummer you have to go, but let's play again soon!";
 
 /**/
 
@@ -16,42 +16,44 @@ var data = [
 
     "Christopher George Latore Wallace earned the notorious nickname 'Big', as an overweight ten year old.",
     "Unbeknownst to his mother, Big began dealing drugs at the age of twelve.",
-    "In 1991, Wallace was nineteen when he was arrested in Raleigh North Carolina for dealing crack cocaine.",
+    "In 1991, Wallace was nineteen when he was arrested in Raleigh, North Carolina for dealing crack cocaine.",
     "Following his arrest in 1991, Notorious B.I.G. pled guilty to possession of cocaine and marijuana with intent to sell.",
-    "Big's entrepreneurship and crack ventures ultimately resulted in a ten year suspended sentence with nine months jail time and subsequent probation.",
-    "Wallace first used the moniker, 'Biggie Smalls', on the demo he made following his release from a North Carolina jail.",
-    "It is the demo Biggie made following his jail sentence in the Carolinas that ultimately led to his recording contract with Uptown Records.",
-    "Biggie Smalls' track 'Ten Crack Commandments,' is featured on 'Life After Death', the artist's posthumously released double album.",
-    "The song 'Ten Crack Commandments' contains a sample of 'Valantra' by Les McCann, and a sample of 'Shut 'Em Down' by Public Enemy.",
+    "Big's entrepreneurial venture ultimately resulted in a suspended sentence with nine months' jail time.",
+    "Wallace first used the moniker, 'Biggie Smalls', on the demo he made following his release.",
+    "The demo that ultimately led to his recording contract with Uptown Records was composed following his time in the Carolinas.",
+    "The track entitled 'Ten Crack Commandments,' is featured on Big's posthumously released double album, 'Life After Death'.",
+    "The track 'Ten Crack Commandments' contains a sample of 'Valantra' by Les McCann.",
+    "Among others, Public Enemy's hit, 'Shut 'Em Down' is sampled on this track.",
     "There are, in fact, ten crack commandments.",
-    "Spending the greater part of his life in the quote unquote, game, Wallace became an animal.",
-    "The 'Ten Crack Commandments' are step-by-step instructions Wallace curated in effort to maximize successes of tradesmen while fending off ill-adjusted hair pieces.",
+    "The 'Ten Crack Commandments' are step-by-step instructions Wallace curated in effort to maximize the successes of tradesmen while fending off ill-adjusted hair pieces.",
     "It should be impossible for anyone to discover the amount of money you have in your possession.",
     "Cheddar breeds jealousy.",
     "If an inebriated associate becomes belligerant, you will likely find yourself in an extremely hostile situation.",
     "Discussing your schedule or disclosing information pertaining to your forthcoming whereabouts will put you in grave danger.",
     "Troublemakers and rapscallions frequently make advances in silence and violence.",
     "No one can be trusted; not even your own mother.",
-    "Your mother will disguise herself, hide in your bushes, and lure you into a rather compromising predicament.",
-    "There should be no recreational use of the merchandise you administer.",
+    "Your mother will hide in your bushes and lure you into a compromising predicament.",
+    "There should never be recreational use of your own merchandise.",
     "No transactions should be made within an eight mile radius of your residence.",
-    "No matter the product or its value, if a potential buyer lives within walking distance of your home, you must bid them farewell.",
+    "No matter the product or its value, if a potential buyer lives nearby, you must bid them farewell.",
     "A crackhead will not adhere to financial obligations.",
     "Incoherent drug addicts seldom have hearts of gold.",
     "Rule number seven: keep your family and your business completely separated.",
     "Felines that nip at your weapons can leap from one location to another with great ease.",
-    "If fellow business associates are under the impression they have been double-crossed, both your kitchen and your livelihood will soon be in disarray.",
+    "If fellow business associates believe they have been double-crossed, both your kitchen and your livelihood will be in disarray.",
     "Cooperation with law enforcement will be done at your own peril.",
-    "Consignment is a business strategy strictly for live men, and not for ninth graders or first year university students.",
-    "Mad clips have been squeezed hitherto, at like-minded gentlemen in order to acquire their bricks and chips.",
-    "Significant sums of money or commodified goods should never be carried on your person.",
+    "Consignment is a business strictly for experienced individuals of a mature age.",
+    "Consignment is not a strategy recommended for ninth graders or first year university students.",
+    "There is controversy between like-minded gentlemen in regards to brick and chip ownership.",
+    "Significant sums of money should never be carried on your person.",
+    "Transpoorting commodified goods should never be done willy nilly.",
     "Rule number one: never let no one know: how much dough you hold.",
     "Rule number two: never let 'em know your next move.",
     "Rule number three: never trust nobody.",
-    "Rule number four: you've heard before: never get high on your own supply.",
-    "Rule number five: never sell no crack where you rest at.",
+    "Rule number four: you've heard before: never fly to the sky on your own supply.",
+    "Rule number five: never exchange rocks for profit where you rest at.",
     "Rule number eight: never keep no weight on you.",
-    "Rule number nine: if you ain't getting bagged, stay away from police.",
+    "Rule number nine: unless it involves a tote bag, there should be no reason to fraternize with law enforcement.",
 
 ];
 
